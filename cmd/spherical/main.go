c11ebbd73f75dcf5e21e0aa9146599d0446b4212
Jake Gealer <jake.gealer@krystal.uk>
2022-10-22 23:19:21 +0100

    generate pgp key on boot

--- cmd/spherical/main.go
+++ cmd/spherical/main.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/ProtonMail/gopenpgp/v2/crypto"
 	"github.com/jakemakesstuff/spherical/db"
 )
 
 func displayVersion() {
-
+	// TODO
 }
 
 func main() {
@@ -31,13 +32,44 @@ func main() {
 	fmt.Println(" success!")
 
 	// Run the database migrations.
-	err = db.Migrate()
-	if err != nil {
+	if err = db.Migrate(); err != nil {
 		panic(err)
 	}
 
 	// If this is just migrations, do not continue any further.
 	if *migrationsOnly {
 		return
+	}
+
+	// Generate or get the PGP key.
+	_, privKeyArmored, err := db.GetPGPKey(func() (pubKey, privKey string) {
+		fmt.Print("[pgp] Generating 4096-bit pgp key - this might take a while...")
+		key, err := crypto.GenerateKey(
+			"Spherical", "noreply.nodegen@spherical.gg", "rsa", 4096)
+		if err != nil {
+			panic(err)
+		}
+		pubKey, err = key.GetArmoredPublicKey()
+		if err != nil {
+			panic(err)
+		}
+		privKey, err = key.Armor()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(" success!")
+		return
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// Load the PGP key in.
+	key, err := crypto.NewKeyFromArmored(privKeyArmored)
+	if err != nil {
+		panic(err)
+	}
+	if !key.IsPrivate() {
+		panic("key is not private")
 	}
 }
---
+++ db/pgp.go
@@ -0,0 +1,69 @@
+package db
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v4"
+)
+
+// GetPGPKey is used to get the PGP key or create it if it doesn't exist.
+func GetPGPKey(maker func() (pubKey, privKey string)) (pubKey, privKey string, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer func() {
+		// In a separate function to stop any references breaking below.
+		cancel()
+	}()
+
+	// Make sure the pgp table exists.
+	c := conn()
+	_, err = c.Exec(ctx, "CREATE TABLE IF NOT EXISTS pgp_key (pub_key TEXT NOT NULL, priv_key TEXT NOT NULL)")
+	if err != nil {
+		return
+	}
+
+	// Get the advisory lock.
+	_, err = c.Exec(ctx, "SELECT pg_advisory_lock(1010)")
+	if err != nil {
+		return
+	}
+	defer func() {
+		_, _ = c.Exec(ctx, "SELECT pg_advisory_unlock(1010)")
+	}()
+
+	// Check if there is a pgp key.
+	fmt.Print("[db] Looking for pgp key...")
+	row := c.QueryRow(ctx, "SELECT pub_key, priv_key FROM pgp_key")
+	if err = row.Scan(&pubKey, &privKey); err != nil {
+		if !errors.Is(err, pgx.ErrNoRows) {
+			// Some other error not relating to it not existing.
+			return
+		}
+
+		// Set the error to nil and log.
+		err = nil
+		fmt.Println(" not found!")
+
+		// Call the maker.
+		pubKey, privKey = maker()
+
+		// Make a new context. It is VERY likely 2 seconds have passed by now.
+		cancel()
+		ctx, cancel = context.WithTimeout(context.Background(), time.Second*2)
+
+		// Write the PGP key to the database.
+		fmt.Print("[db] Writing new pgp key...")
+		_, err = c.Exec(ctx, "INSERT INTO pgp_key (pub_key, priv_key) VALUES ($1, $2)", pubKey, privKey)
+		if err != nil {
+			return
+		}
+		fmt.Println(" success!")
+
+		// Return here.
+		return
+	}
+	fmt.Println(" found!")
+	return
+}
